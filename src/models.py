eabd3c4eb0563dfbf8dcee9502e3464f59a88201
Sophia Descho <descho@cip.ifi.lmu.de>
2018-11-09 17:55:15 +0100

    use snakecase

--- src/models.py
+++ src/models.py
@@ -18,13 +18,13 @@ class TempTP(db.Model):
     graduation = db.Column(db.String(100), nullable=False)
     apprenticeship = db.Column(db.String(100))
     timestamp = db.Column(db.TIMESTAMP, nullable=False) #needed?
-    languageTable = db.relationship('Languages', backref='temptp', lazy=True)
-    studiesTable = db.relationship('Studies', backref='temptp', lazy=True)
-    itSkillsTable = db.relationship('ItSkills', backref='temptp', lazy=True)
-    furtherEducationTable = db.relationship('ItSkills', backref='temptp', lazy=True)
-    professionalExpTable = db.relationship('ProfessionalExperience', backref='temptp', lazy=True)
-    applicationsTable = db.relationship('Applications', backref='temptp', lazy=True)
-    leadershipTable=db.relationship('Leading_Position', backref='temptp', lazy=True)
+    languageTable = db.relationship('languages', backref='temptp', lazy=True)
+    studiesTable = db.relationship('studies', backref='temptp', lazy=True)
+    itSkillsTable = db.relationship('it_skills', backref='temptp', lazy=True)
+    furtherEducationTable = db.relationship('further_education', backref='temptp', lazy=True)
+    professionalExpTable = db.relationship('professional_experience', backref='temptp', lazy=True)
+    applicationsTable = db.relationship('applications', backref='temptp', lazy=True)
+    leadershipTable = db.relationship('leading_positions', backref='temptp', lazy=True)
     careerGoals = db.Column(db.Text)
     salaryExpectations = db.Column(db.Integer(40))
     salaryFactors = db.Column(db.Text)
@@ -36,7 +36,7 @@ class TempTP(db.Model):
 
 
 class LeadingPositions(db.Model):
-    __tablename__ = 'Leading_Positions'
+    __tablename__ = 'leading_positions'
     id = db.Column(db.Integer, primary_key=True)
     previousLeadingPosition = db.Column(db.String(100))
     nrOfPplResponsible = db.Column(db.Integer, nullable=False)
@@ -44,14 +44,14 @@ class LeadingPositions(db.Model):
 
 
 class Applications(db.Model):
-    __tablename__ = 'Applications'
+    __tablename__ = 'applications'
     id = db.Column(db.Integer, primary_key=True)
     company = db.Column(db.String(100), nullable=False)
     temptp_id = db.Column(db.Integer, db.ForeignKey('temptp.id'), nullable=False)
 
 
 class ProfessionalExperience(db.Model):  # what if you have no Experience?
-    __tablename__ = 'Professional_Experience'
+    __tablename__ = 'professional_experience'
     id = db.Column(db.Integer, primary_key=True)
     company = db.Column(db.String(100))
     position = db.Column(db.String(100))
@@ -61,14 +61,14 @@ class ProfessionalExperience(db.Model):  # what if you have no Experience?
 
 
 class FurtherEducation(db.Model):  # what if you have no further education?
-    __tablename__ = 'Further_Education'
+    __tablename__ = 'further_education'
     id = db.Column(db.Integer, primary_key=True)
     education = db.Column(db.String(100))
     temptp_id = db.Column(db.Integer, db.ForeignKey('temptp.id'), nullable=False)
 
 
 class Studies(db.Model):  # Studium
-    __tablename__ = 'Studies'
+    __tablename__ = 'studies'
     id = db.Column(db.Integer, primary_key=True)
     studies = db.Column(db.String(100))
     periodFrom = db.Column(db.Date, nullable=False)
@@ -77,7 +77,7 @@ class Studies(db.Model):  # Studium
 
 
 class ItSkills(db.Model):
-    __tablename__ = 'IT_Skills'
+    __tablename__ = 'it_skills'
     id = db.Column(db.Integer, primary_key=True)
     itSkills = db.Column(db.String(100))
     knowledge = db.Column(db.Integer)  # how good is your skill
@@ -85,7 +85,7 @@ class ItSkills(db.Model):
 
 
 class Languages(db.Model):
-    __tablename__ = 'Languages'
+    __tablename__ = 'languages'
     id = db.Column(db.Integer, primary_key=True)
     language = db.Column(db.String(80), nullable=False)
     # knowledge how good you speak it:
